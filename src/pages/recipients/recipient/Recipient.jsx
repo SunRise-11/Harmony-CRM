c3b9d171d4b77c40a054e4a5f74e67b73a38182c
newheaven918 <new.heaven918@gmail.com>
2024-02-20 13:56:32 -0500

    completed to update the tabbar.

--- src/pages/recipients/recipient/Recipient.jsx
+++ src/pages/recipients/recipient/Recipient.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ConfigProvider, Tabs } from "antd";
 import InlineSVG from "react-inlinesvg";
 import homeSVG from "../../../assets/icons/home.svg";
@@ -24,73 +24,137 @@ import Employment from "./employment/Employment";
 import Questionnaires from "./questionnaires";
 
 const Recipient = () => {
+  const [activeKey, setActiveKey] = useState("1");
   const tabs = [
     {
       key: "1",
       label: "דף הבית",
       children: <Home />,
-      icon: <InlineSVG src={homeSVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={homeSVG}
+          width={20}
+          style={activeKey === "1" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
     {
       key: "2",
       label: "פרופיל",
       children: <Profile />,
-      icon: <InlineSVG src={profileSVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={profileSVG}
+          width={20}
+          style={activeKey === "2" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
     {
       key: "3",
       label: "תיק רפואי",
       children: <Medical />,
-      icon: <InlineSVG src={medicalSVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={medicalSVG}
+          width={20}
+          style={activeKey === "3" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
     {
       key: "4",
       label: "מסמכים",
       children: <Documents />,
-      icon: <InlineSVG src={attachSVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={attachSVG}
+          width={20}
+          style={activeKey === "4" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
     {
       key: "5",
       label: "אירועים",
       children: <Events />,
-      icon: <InlineSVG src={eventsSVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={eventsSVG}
+          width={20}
+          style={activeKey === "5" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
     {
       key: "6",
       label: "יומן פגישות",
       children: <DailyPlanner />,
-      icon: <InlineSVG src={planSVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={planSVG}
+          width={20}
+          style={activeKey === "6" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
     {
       key: "7",
       label: "משימות",
       children: <Tasks />,
-      icon: <InlineSVG src={taskSVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={taskSVG}
+          width={20}
+          style={activeKey === "7" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
     {
       key: "8",
       label: "לימודים",
       children: <Studies />,
-      icon: <InlineSVG src={studySVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={studySVG}
+          width={20}
+          style={activeKey === "8" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
     {
       key: "9",
       label: "תעסוקה",
       children: <Employment />,
-      icon: <InlineSVG src={jobSVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={jobSVG}
+          width={20}
+          style={activeKey === "9" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
     {
       key: "10",
       label: "שאלונים",
       children: <Questionnaires />,
-      icon: <InlineSVG src={questionSVG} width={20} />,
+      icon: (
+        <InlineSVG
+          src={questionSVG}
+          width={20}
+          style={activeKey === "10" ? { stroke: "#1677ff" } : {}}
+        />
+      ),
     },
   ];
+
   return (
     <ConfigProvider direction="rtl">
       <Tabs
         className="recipient"
         defaultActiveKey="1"
+        activeKey={activeKey}
+        onTabClick={(data) => setActiveKey(data)}
         items={tabs}
         tabBarStyle={{
           backgroundColor: "white",
