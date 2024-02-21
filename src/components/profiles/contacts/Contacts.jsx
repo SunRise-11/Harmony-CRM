4b55ddbff31d2eca68068658c732fc5927b7dc65
newheaven918 <new.heaven918@gmail.com>
2024-02-21 09:08:21 -0500

    completed to make the contacts profile.

--- src/components/profiles/contacts/Contacts.jsx
+++ src/components/profiles/contacts/Contacts.jsx
@@ -1,5 +1,54 @@
-const Contacts = () => {
-  return <div>Contacts</div>;
+import Basic from "../basic/Basic";
+import { Button } from "antd";
+import usersSVG from "../../../assets/icons/users.svg";
+import addImg from "../../../assets/icons/add.svg";
+import Item from "../basic/item/Item";
+import InlineSVG from "react-inlinesvg";
+
+const Contacts = ({ selected }) => {
+  return (
+    <Basic
+      title="אנשי קשר"
+      icon={usersSVG}
+      selected={selected}
+      content={
+        <>
+          <Item type="dropdown" width="200px" required text="שם מלא" />
+          <Item type="input" width="200px" required text="טלפון" />
+          <Item type="input" width="200px" required text="טלפון נייד" />
+          <Item type="input" width="640px" text="דואר אלקטרוני" />
+          <Item type="dropdown" width="640px" required text="מסגרת מפנה" />
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              gap: "10px",
+              paddingLeft: "10px",
+            }}
+          >
+            <Button
+              style={{
+                borderRadius: "32px",
+                padding: "2px",
+                width: "32px",
+                height: "32px",
+                border: "1px solid #CFD3D9",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "white",
+              }}
+            >
+              <InlineSVG src={addImg} />
+            </Button>
+            <div>הוסף איש קשר</div>
+          </div>
+        </>
+      }
+    />
+  );
 };
 
 export default Contacts;
--- src/components/profiles/education/Education.jsx
+++ src/components/profiles/education/Education.jsx
@@ -1,5 +1,35 @@
-const Education = () => {
-  return <div>Education</div>;
+import Basic from "../basic/Basic";
+import geometricsSVG from "../../../assets/icons/geometrics.svg";
+import Item from "../basic/item/Item";
+
+const Education = ({ selected }) => {
+  return (
+    <Basic
+      title="אנשי קשר"
+      icon={geometricsSVG}
+      selected={selected}
+      content={
+        <>
+          <Item type="dropdown" width="200px" required text="ישוב" />
+          <Item type="input" width="200px" required text="רחוב" />
+          <Item type="input" width="200px" required text="מספר" />
+          <Item type="input" width="640px" text="הערות" />
+          <div
+            style={{
+              paddingBottom: "10px",
+              borderBottom: "1px solid #CED8DB",
+              width: "100%",
+            }}
+          ></div>
+          <Item type="input" width="200px" required text="טלפון נייד" />
+          <Item type="input" width="200px" text="טלפון בבית" />
+          <Item type="input" width="200px" text="טלפון נוסף" />
+
+          <Item type="input" width="640px" required text="דואר אלקטרוני" />
+        </>
+      }
+    />
+  );
 };
 
 export default Education;
