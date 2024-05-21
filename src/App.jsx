9efcc280943aab4fddf598744f4b87b4428633be
newheaven918 <new.heaven918@gmail.com>
2024-05-21 05:06:49 -0400

    Change app.js to fix issue 20

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Provider } from "react-redux";
 import { Helmet } from "react-helmet";
 import { store } from "./redux/store";
@@ -7,31 +7,64 @@ import "./App.scss";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const App = () => {
-  useEffect(() => {
-    const date = new Date();
-    const hours = date.getHours();
-    const icons = ["blue", "red", "yellow", "green"];
-    
-    const faviconElement = document.getElementById("dynamic-favicon");
-    if (faviconElement) {
-        faviconElement.setAttribute("href", `icons/${icons[hours % 4]}.svg`);
+  // useEffect(() => {
+  //   const date = new Date();
+  //   const hours = date.getHours();
+  //   const icons = ["blue", "red", "yellow", "green"];
+
+  //   const faviconElement = document.getElementById("dynamic-favicon");
+  //   if (faviconElement) {
+  //       faviconElement.setAttribute("href", `icons/${icons[hours % 4]}.svg`);
+  //   }
+  // }, []);
+
+  // return (
+  //   <>
+  //     <Helmet>
+  //       <link
+  //         id="dynamic-favicon"
+  //         rel="icon"
+  //         type="image/x-icon"
+  //         href="icons/blue.svg"
+  //       />
+  //     </Helmet>
+  //     <Provider store={store}>
+  //       <RoutesDefined />
+  //     </Provider>
+  //   </>
+  // );
+
+  const [menuVisible, setMenuVisible] = useState(false);
+  const textboxRef = useRef(null);
+
+  const loseFocus = () => {
+    if (textboxRef.current) {
+      textboxRef.current.blur();
     }
-  }, []);
+    setMenuVisible(!menuVisible);
+  };
 
   return (
-    <>
-      <Helmet>
-        <link
-          id="dynamic-favicon"
-          rel="icon"
-          type="image/x-icon"
-          href="icons/blue.svg"
-        />
-      </Helmet>
-      <Provider store={store}>
-        <RoutesDefined />
-      </Provider>
-    </>
+    <div>
+      <div id="menuBar">
+        <div id="showMenu" onClick={loseFocus}>
+          ☰
+        </div>
+        {menuVisible && (
+          <div id="menu">
+            <ul>
+              <li>Menu Option 1</li>
+              <li>Menu Option 2</li>
+              <li>Menu Option 3</li>
+            </ul>
+          </div>
+        )}
+      </div>
+      <div id="content">
+        <label htmlFor="textbox">Text:</label>
+        <input id="textbox" type="text" ref={textboxRef} />
+      </div>
+    </div>
   );
 };
 
